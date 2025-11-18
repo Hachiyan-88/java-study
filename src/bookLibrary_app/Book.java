384990d26dfc5fbecc57264dad541b6e28f0ca4c
Hachiyan-88 <T.Hachiya@vision-sys.co.jp>
2025-11-18 13:17:14 +0900

    feature/booklibrary_app

---
+++ src/bookLibrary_app/Book.java
@@ -0,0 +1,29 @@
+package bookLibrary_app;
+
+public class Book {
+	
+	private int id;
+	private String title;
+	private String author;
+	
+	//コンストラクタ
+	public Book(int id, String title, String author) {
+		
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+
+}
---
+++ src/bookLibrary_app/package-info.java
@@ -0,0 +1,1 @@
+package bookLibrary_app;
